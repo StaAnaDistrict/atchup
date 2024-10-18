edf40de11c8a88ffdc6db35a68350203e52c26a7
StaAnaDistrict <104166106+StaAnaDistrict@users.noreply.github.com>
2024-10-18 16:59:22 +0800

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const https = require('https');
 
 exports.handler = async (event, context) => {
   console.log('Function invoked');
@@ -14,35 +14,54 @@ exports.handler = async (event, context) => {
   // URL of your Google Apps Script web app
   const scriptUrl = 'https://script.google.com/macros/s/AKfycby4S4V-MEMDFDM46FUdb1E61GJAdFfLf2GNUsYZGz0ZPjlxwhkExRgPhxG8AOaRCsJf/exec';
 
-  try {
-    console.log('Sending request to Google Apps Script');
-    const response = await fetch(scriptUrl, {
+  return new Promise((resolve, reject) => {
+    const req = https.request(scriptUrl, {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }, (res) => {
+      let data = '';
 
-    const data = await response.json();
-    console.log('Received response from Google Apps Script:', data);
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
 
-    if (data.result === 'success') {
-      console.log('Login successful');
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ result: 'success' })
-      };
-    } else {
-      console.log('Login failed');
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ result: 'failure' })
-      };
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ result: 'error', message: error.message })
-    };
-  }
+      res.on('end', () => {
+        console.log('Received response from Google Apps Script:', data);
+        try {
+          const parsedData = JSON.parse(data);
+          if (parsedData.result === 'success') {
+            console.log('Login successful');
+            resolve({
+              statusCode:
+              200,
+body: JSON.stringify({ result: 'success' })
+});
+} else {
+console.log('Login failed');
+resolve({
+statusCode: 401,
+body: JSON.stringify({ result: 'failure' })
+});
+}
+} catch (error) {
+console.error('Error parsing response:', error);
+reject({
+statusCode: 500,
+body: JSON.stringify({ result: 'error', message: 'Error parsing response' })
+});
+}
+});
+});
+req.on('error', (error) => {
+console.error('Error:', error);
+reject({
+statusCode: 500,
+body: JSON.stringify({ result: 'error', message: error.message })
+});
+});
+req.write(JSON.stringify({ email, password }));
+req.end();
+});
 };
